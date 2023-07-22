e7f5cf94b13729eec800c0ceb2494d0b79694cb7
jaehoon <jaehoon@jaehoons-MacBook-Pro.local>
2023-07-22 13:08:32 +0900

    2023-07-22: 오전 학습 내용 커밋

--- helloboot/src/main/java/me/jaehoon/helloboot/HellobootApplication.java
+++ helloboot/src/main/java/me/jaehoon/helloboot/HellobootApplication.java
@@ -1,11 +1,37 @@
 package me.jaehoon.helloboot;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
-@SpringBootApplication
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 public class HellobootApplication {
     public static void main(String[] args) {
-        SpringApplication.run(HellobootApplication.class, args);
+        // UnderTow, Jetty로도 커스터마이징하여 사용이 가능하다.
+        ServletWebServerFactory tomcatServletWebServerFactory = new TomcatServletWebServerFactory();
+        WebServer webServer = tomcatServletWebServerFactory.getWebServer(servletContext -> {
+            servletContext.addServlet("hello", new HttpServlet() {
+                @Override
+                protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                    String name = req.getParameter("name");
+
+                    resp.setStatus(HttpStatus.OK.value());
+                    resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+                    resp.getWriter().println("Hello " + name);
+                }
+            }).addMapping("/hello");
+        });
+
+        webServer.start();
     }
 }
